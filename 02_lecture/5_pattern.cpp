f565be0e4e9b604b6bea7ef8991ef469c18a04f8
Rishabhkhatarkar2005 <reeshabhkhatarkar@gmail.com>
2026-02-03 13:51:36 +0530

    notes

---
+++ 02_lecture/5_pattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < 0; i++)
+    {
+        for (int j = 0; j <= i - 1; i++)
+        {
+            cout << "";
+            for (int j = 0; j <= n - i + 1; j++)
+            {
+                cout << "*"
+            }
+        }
+    }
