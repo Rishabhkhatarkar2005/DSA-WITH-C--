1e458ccd3ab609c5abff478bdb2c1353643453dc
Rishabhkhatarkar2005 <reeshabhkhatarkar@gmail.com>
2026-02-04 22:38:33 +0530

    notes

---
+++ 15_lecture/01_febonaci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fibbo(int g)
+{
+    if (g == 0 or g == 1)
+    {
+        return g;
+    }
+    return fibbo(g - 1) + fibbo(g - 2);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibbo(n) << endl;
+    return 0;
+}
