ef3dc07d15256c3f93d5778d50e00465b666e571
Rishabhkhatarkar2005 <reeshabhkhatarkar@gmail.com>
2026-02-10 23:02:07 +0530

    notes

--- 02_lecture/03_pattern.cpp
+++ 02_lecture/03_pattern.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=0;j--){
-            cout<<"*"
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= 0; j--)
+        {
+            cout << "*";
         }
     }
 }
