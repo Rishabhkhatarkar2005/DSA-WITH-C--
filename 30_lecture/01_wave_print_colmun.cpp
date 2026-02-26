8db11b700be8184bd17dd575e6f84816983a1b7e
Rishabhkhatarkar2005 <reeshabhkhatarkar@gmail.com>
2026-02-26 13:44:30 +0530

    notes

--- 30_lecture/01_wave_print_colmun.cpp
+++ 30_lecture/01_wave_print_colmun.cpp
@@ -1,3 +1,44 @@
-#include <ostream>
+#include <iostream>
 #include <vector>
 using namespace std;
+
+int main()
+{
+    int r, c;
+    cin >> r >> c;
+
+    vector<vector<int>> arr(r, vector<int>(c));
+
+    // input
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    // snake column print
+    for (int j = 0; j < c; j++)
+    {
+
+        // even column → top to bottom
+        if (j % 2 == 0)
+        {
+            for (int i = 0; i < r; i++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        // odd column → bottom to top
+        else
+        {
+            for (int i = r - 1; i >= 0; i--)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+    }
+
+    return 0;
+}
